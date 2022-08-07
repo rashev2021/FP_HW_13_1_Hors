e5753199095c5fd8ed6f79a56e63c6c5e1b7ecc8
App_Shop <dvina400@gmail.com>
2022-08-07 11:11:32 +0300

    Добавьте файлы проекта.

---
+++ FP_HW_13_1/FP_HW_13_1.cpp
@@ -0,0 +1,67 @@
+﻿#include <Windows.h>
+#include <iostream>
+
+using namespace std;
+
+int const sizeX = 8;
+int const sizeY = 8;
+int x, y;
+
+int arrayDesk[sizeX][sizeY];
+
+void motionHors(int x, int y) {
+	
+	cout << endl;
+	for (int i = 0; i < sizeX; i++) {
+		cout << " ";
+		for (int j = 0; j < sizeY; j++) {
+			cout << "[" << " " << "]";
+
+			if (i == y && j == x - 1) {
+			
+				cout << "[" << "#" << "]";
+			}
+		}
+		cout << endl;
+	}
+
+}
+
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	//Игра: Ход коня
+
+	cout << endl << " Шахматная доска" << endl << endl;
+	for (int i = 0; i < sizeX; i++) {
+		cout << " ";
+		for (int j = 0; j < sizeY; j++) {
+			cout << "[" << " " << "]";
+		}
+		cout << endl;
+	}
+
+	cout << endl << " Поставьте коня. Введите координаты: " << endl;
+	cin >> x >> y;
+	motionHors(x,y);
+
+	do {
+		cout << endl << " Сделайте ход конем. Введите координаты: " << endl;
+		cin >> x >> y;
+
+		if (y > 4 && x > 4) {
+			cout << " Так конь не ходит";
+			continue;
+		}
+		if (y <= 3 && x <= 3) {
+			cout << " Так конь не ходит";
+			continue;
+		}
+		motionHors(x, y);
+	} while (true);
+
+}
+
